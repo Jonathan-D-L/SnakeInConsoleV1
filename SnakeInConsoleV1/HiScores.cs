a44c9d3b830b2a4c0218926c6b87e4e500b13b43
Jonathan.Donath.Lopez <Jonathan.Donath.Lopez@hotmail.com>
2022-11-27 00:04:25 +0100

    upt

--- SnakeInConsoleV1/HiScores.cs
+++ SnakeInConsoleV1/HiScores.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Spectre.Console;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace SnakeInConsoleV1
 {
@@ -22,27 +24,41 @@ namespace SnakeInConsoleV1
             return scores;
 
         }
-        public void AddHiScore(string playerName, int score, int difficulty)
+        public void AddHiScore(string playerName, int playerScore, int difficulty)
         {
             var directory = "Files";
             var fileName = "Hiscores.txt";
-            if(difficulty == 0)
+            if (difficulty == 0)
             {
-                score *= 1;
+                playerScore *= 1;
             }
-            if(difficulty == 1)
+            if (difficulty == 1)
             {
-                score = Convert.ToInt32(Math.Floor(score * 1.5));
+                playerScore = Convert.ToInt32(Math.Floor(playerScore * 1.5));
             }
-            if(difficulty == 2)
+            if (difficulty == 2)
             {
-                score *= 2;
+                playerScore *= 2;
+            }
+            var scores = new List<string>();
+            foreach (var line in File.ReadLines($"{directory}\\{fileName}"))
+            {
+                scores.Add(line);
+            }
+            foreach (var s in scores)
+            {
+                var name = s.Split('|')[0];
+                var score = s.Split('|')[1];
+                if (playerName == name && playerScore > Convert.ToInt32(score))
+                {
+                    scores.Remove(score.Where(s => s == name))..;
+                }
             }
             if (File.Exists($"{directory}\\{fileName}"))
             {
                 if (playerName != string.Empty)
                 {
-                    var playerNameAndScore = $"{playerName}|{score}\r\n";
+                    var playerNameAndScore = $"{playerName}|{playerScore}\r\n";
                     File.AppendAllText($"{directory}\\{fileName}", playerNameAndScore);
                 }
             }
